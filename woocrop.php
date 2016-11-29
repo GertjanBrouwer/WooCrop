5b81987140e2acce2c57c4ef49705afbc6b53566
Gertjan Brouwer <gertjanbr@hotmail.com>
2016-11-29 14:16:02 +0100

    Changed documentation

--- woocrop.php
+++ woocrop.php
@@ -15,8 +15,8 @@
 	 * License: GNU General Public License v3.0
 	 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
 	 */
-	
+
 	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	    // Put your plugin code here
+	    
 	}
 ?>
