1e4557325305879c165cb4235b14e1a8cc2cb0ec
Gertjan Brouwer <gertjanbr@hotmail.com>
2016-11-29 14:11:19 +0100

    created header information and added file structure

---
+++ woocrop.php
@@ -0,0 +1,22 @@
+<?php
+	/*
+	 * Plugin Name: WooCrop
+	 * Plugin URI: https://github.com/GertjanBrouwer/WooCrop
+	 * Description: Allows
+	 * Version: 1.0.0
+	 * Author: WooCommerce
+	 * Author URI: http://woocommerce.com/
+	 * Developer: Gertjan Brouwer
+	 * Developer URI: https://github.com/GertjanBrouwer/
+	 * Text Domain: woocrop
+	 * Domain Path: /languages
+	 *
+	 * Copyright: © 2009-2015 WooCommerce.
+	 * License: GNU General Public License v3.0
+	 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+	 */
+	
+	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	    // Put your plugin code here
+	}
+?>
