83e37c76ff15c6343bbe6e39c7376d3b2b92c764
Gertjan Brouwer <gertjanbr@hotmail.com>
2016-12-04 00:08:33 +0100

    Modified todo list Added upload field to woocrop

--- woocrop.php
+++ woocrop.php
@@ -16,74 +16,44 @@
 	 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
 	 */
 
+	//Don't allow direct calls to woocrop.php file
+	if ( ! defined( 'ABSPATH' ) ) {
+		exit; // Exit if accessed directly.
+	}
+
+	//Checks if woocommerce is active
 	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	    
-	    // function save_name_on_tshirt_field( $cart_item_data, $product_id ) {
-	    // 	global $woocommerce;
-	    //     if( isset( $_COOKIE['prijs_cookie'] ) && $product_id == 222) {
-	    //         $cart_item_data['prijs_voor_product'] = $_COOKIE['prijs_opslag'];
-	    //          below statement make sure every add to cart action as unique line item 
-	    //         $cart_item_data['unique_key'] = md5( microtime().rand() );
-	    //     }
-	    //     return $cart_item_data;
-	    // }
-	    // add_action( 'woocommerce_add_cart_item_data', 'save_name_on_tshirt_field', 10, 2 );
+	    //Puts the crop area in the right location
+	    add_action('woocommerce_after_single_product', 'woocrop_add_crop_area');
+	    function woocrop_add_crop_area(){
+	    	//Product page(s) where crop area needs to be displayed
+	    	if(is_product('8'))	{
+	    		?>
+	    		<div id="woocrop_value_area">
+		    		<!-- These inputs hold values for cropping -->
+		    		<!-- <input type="hidden" id="price" name="price" /> -->
+				    <input type="hidden" id="woocrop_min_width" name="min_width" value="" />
+				    <input type="hidden" id="woocrop_min_height" name="min_height" value="" />
+				    <input type="hidden" id="woocrop_thumbnail_width" name="thumbnail_width" value="" />
+				    <input type="hidden" id="woocrop_thumbnail_height" name="thumbnail_height" value="" />
+				    <input type="hidden" id="woocrop_orig_width" name="orig_width" value="" />
+				    <input type="hidden" id="woocrop_orig_height" name="orig_height" value="" />
+				    <input type="hidden" id="woocrop_achtergrond_link" name="achtergrond_link" value="" />
 
-		// function woo_add_prijs() {
-		//     global $woocommerce;
-
-		//     foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-		//         if($cart_item['data']->id == 222 && ! empty($_COOKIE['prijs_cookie'])){
-		//             $cart_item['data']->set_price($_COOKIE['prijs_cookie']);
-		//         }
-		//     }
-		// }
-
-		// add_action( 'woocommerce_before_calculate_totals', 'woo_add_prijs', 10, 0);
-
-	    function save_gift_wrap_fee( $cart_item_data, $product_id ) {
-	         
-	        if( isset( $_COOKIE['prijs_cookie'] ) && $product_id == 222 ) {
-	        	$prijs_voor_productVar = $_COOKIE['prijs_cookie'];
-	        	$afbeelding_url_productVar = $_COOKIE['afbeelding_url'];
-	            $cart_item_data[ "prijs_voor_product" ] = $prijs_voor_productVar;  
-	            $cart_item_data[ "afbeelding_url_product" ] =  $afbeelding_url_productVar; 
-	        }
-	        return $cart_item_data;
-	         
+				    <!-- These inputs hold the location of the crop section for calculating the thumbnail see function showPreview-->
+				    <input type="hidden" id="woocrop_x1" name="woocrop_x1" />
+			        <input type="hidden" id="woocrop_y1" name="woocrop_y1" />
+			        <input type="hidden" id="woocrop_x2" name="woocrop_x2" />
+			        <input type="hidden" id="woocrop_y2" name="woocrop_y2" />
+			        <input type="hidden" id="woocrop_w" name="woocrop_width" />
+			        <input type="hidden" id="woocrop_h" name="woocrop_height" />
+			    </div>
+			    <div id="woocrop_crop_area">
+			    	<input type="file" name="image_file" id="upload-image" accept="image/*" />
+			        <input type="button" value="Kies afbeelding" id="image_event" onclick="document.getElementById('upload-image').click();" />
+			    </div>
+		        <?php
+	    	}
 	    }
-	    add_filter( 'woocommerce_add_cart_item_data', 'save_gift_wrap_fee', 99, 2 );
-
-	    function calculate_prijs($cart_object) {
-	    	global $woocommerce;  
-	        if( !WC()->session->__isset( "reload_checkout" )) {
-	            //$additionalPrice = $_COOKIE['prijs_cookie'];
-	            //echo($_COOKIE['prijs_cookie']);
-	            foreach ( $cart_object->cart_contents as $key => $value ) {
-	                if( isset( $value["prijs_voor_product"] ) ) {
-	                	$orgPrice = $value["prijs_voor_product"];
-	                    $value['data']->price = ( $orgPrice );
-	                    //echo($additionalPrice);
-	                }
-	                else{
-	                	//echo("geen cart");
-	                }
-	            }   
-	        }   
-	    }
-	    add_action( 'woocommerce_before_calculate_totals', 'calculate_prijs', 99, 1);
-
-	    function render_meta_on_cart_and_checkout( $cart_data, $cart_item = null ) {
-	        $custom_items = array();
-	        /* Woo 2.4.2 updates */
-	        if( !empty( $cart_data ) ) {
-	            $custom_items = $cart_data;
-	        }
-	        if( isset( $cart_item['afbeelding_url_product'] ) ) {
-	            $custom_items[] = array( "name" => 'Afbeelding', "value" => $cart_item['afbeelding_url_product'] );
-	        }
-	        return $custom_items;
-	    }
-	    add_filter( 'woocommerce_get_item_data', 'render_meta_on_cart_and_checkout', 10, 2 );
 	}
 ?>
